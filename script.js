aa98927c2eb631b65c1842d9108cca0e9d720b25
rediahmds_ubuntu <redi.ngoding@gmail.com>
2024-05-10 02:07:04 +0700

    change the geolocation provider once more

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ window.addEventListener('DOMContentLoaded', async () => {
         const apiKey = userApiKey || defaultApiKey;
 
         // Getting current user location
-        const geoLocation = 'http://ip-api.com/json/';
+        const geoLocation = 'https://ipapi.co/json/';
         const locationData = await fetch(geoLocation);
         const parsedLocation = await locationData.json();
-        const currentUserLocation = parsedLocation.query;
+        const currentUserLocation = parsedLocation.ip;
 
         const weatherApi = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${currentUserLocation}&aqi=no`;
 
