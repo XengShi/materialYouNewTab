b13a8ee037c1cb91f8684cbedcd4625157201814
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2024-11-19 22:15:50 +0530

    Better quality Github shortcut logo (#193)
    * Update script.js
    
    Github logo link
    
    * Add files via upload
    
    * Update script.js
    
    svg
    
    * Add files via upload
    
    * Delete shortcuts_icons/github.png

--- script.js
+++ script.js
@@ -1863,7 +1863,11 @@ document.addEventListener("DOMContentLoaded", function () {
         const logo = document.createElement("img");
 
         const hostname = new URL(urlString).hostname;
-        logo.src = GOOGLE_FAVICON_API_FALLBACK(hostname);
+        if (hostname === "github.com") {
+            logo.src = "./shortcuts_icons/github-shortcut.svg";
+        } else {
+            logo.src = GOOGLE_FAVICON_API_FALLBACK(hostname);
+        }
 
         return logo;
     }
