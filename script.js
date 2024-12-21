711c22dcb81f05e3d52b42b258cf91378de47658
Shivansh <68265106+Thunder-Blaze@users.noreply.github.com>
2024-12-21 20:54:12 +0530

    Feature: Added Pinning Functionality to ToDo List (#337)
    * Added Pinning Functionality to ToDo List
    
    * Renamed Todo Span and Fixed Minor Indentation Issues
    
    * Update script.js
    
    darktheme removebtn hover
    
    * Update style.css
    
    minor adjustments
    
    * Update style.css
    
    * Update style.css
    
    * Update style.css
    
    font size 17 to 16
    
    * Updated Todo SVGs
    
    * Added No deletion of Pending Tasks and Resetting of Pinned Completed tasks
    
    ---------
    
    Co-authored-by: Prem Kumar <60751338+prem-k-r@users.noreply.github.com>

--- script.js
+++ script.js
@@ -303,23 +303,29 @@ function addtodoItem() {
     }
     const t = "t" + Date.now(); // Generate a Unique ID
     const rawText = inputText;
-    todoList[t] = { title: rawText, status: "pending" }; // Add data to the JSON variable
-    const li = createTodoItemDOM(t, rawText, "pending"); // Create List item
+    todoList[t] = { title: rawText, status: "pending", pinned: false }; // Add data to the JSON variable
+    const li = createTodoItemDOM(t, rawText, "pending", false); // Create List item
     todoulList.appendChild(li); // Append the new item to the DOM immediately
     todoInput.value = ''; // Clear Input
     SaveToDoData(); // Save changes
 }
 
-function createTodoItemDOM(id, title, status) {
+function createTodoItemDOM(id, title, status, pinned) {
     let li = document.createElement('li');
     li.innerHTML = sanitizeInput(title); // Sanitize before rendering in DOM
-    const span = document.createElement("span"); // Create the Cross Icon
-    span.setAttribute("class", "todoremovebtn");
-    span.textContent = "\u00d7";
-    li.appendChild(span); // Add the cross icon to the LI tag
+    const removebtn = document.createElement("span"); // Create the Cross Icon
+    removebtn.setAttribute("class", "todoremovebtn");
+    removebtn.textContent = "\u00d7";
+    li.appendChild(removebtn); // Add the cross icon to the LI tag
     li.setAttribute("class", "todolistitem");
     if (status === 'completed') {
         li.classList.add("checked");
+    }
+    const pinbtn = document.createElement("span"); // Create the Cross Icon
+    pinbtn.setAttribute("class", "todopinbtn");
+    li.appendChild(pinbtn); // Add the cross icon to the LI tag
+    if (pinned) {
+        li.classList.add("pinned");
     }
     li.setAttribute("data-todoitem", id); // Set a data attribute to the li so that we can uniquely identify which li has been modified or deleted
     return li; // Return the created `li` element
@@ -332,10 +338,15 @@ todoulList.addEventListener("click", (event) => {
         let id = event.target.dataset.todoitem;
         todoList[id].status = ((todoList[id].status === "completed")? "pending" : "completed"); // Update status
         SaveToDoData(); // Save Changes
-    } else if (event.target.tagName === "SPAN"){
+    } else if (event.target.classList.contains('todoremovebtn')){
         let id = event.target.parentElement.dataset.todoitem;
         event.target.parentElement.remove(); // Remove the clicked LI tag
         delete todoList[id]; // Remove the deleted List item data
+        SaveToDoData(); // Save Changes
+    } else if (event.target.classList.contains('todopinbtn')){
+        event.target.parentElement.classList.toggle("pinned"); // Check the clicked LI tag
+        let id = event.target.parentElement.dataset.todoitem;
+        todoList[id].pinned = ((todoList[id].pinned === true)? false : true); // Update status
         SaveToDoData(); // Save Changes
     }
 });
@@ -351,7 +362,7 @@ function ShowToDoList() {
         const fragment = document.createDocumentFragment(); // Create a DocumentFragment
         for (let id in todoList) {
             const todo = todoList[id];
-            const li = createTodoItemDOM(id, todo.title, todo.status); // Create `li` elements
+            const li = createTodoItemDOM(id, todo.title, todo.status, todo.pinned); // Create `li` elements
             fragment.appendChild(li); // Add `li` to the fragment
         }
         todoulList.appendChild(fragment); // Append all `li` to the `ul` at once
@@ -367,9 +378,19 @@ let todoCurrentDate = new Date().toLocaleDateString(); // Get current date
 if (todoLastUpdateDate===todoCurrentDate){
     ShowToDoList();
 } else {
-    // Reset the list when last update date and the current date does not match
+    // Modify the list when last update date and the current date does not match
     localStorage.setItem("todoLastUpdateDate",todoCurrentDate);
-    localStorage.setItem("todoList",'{}');
+    todoList = JSON.parse(localStorage.getItem("todoList")) || {};
+    for(let id in todoList){
+        if (todoList[id].pinned == false){
+            if (todoList[id].status == "completed") {
+                delete todoList[id]; // Remove the Unpinned and Completed list item data
+            }
+        } else {
+            todoList[id].status = "pending"; // Reset status of pinned items
+        }
+    }
+    SaveToDoData();
     ShowToDoList();
 }
 
@@ -1324,7 +1345,7 @@ const applySelectedTheme = (colorValue) => {
             }
 
             .dark-theme #searchQ {
-            color: #fff;
+                color: #fff;
             }
 
             .dark-theme .searchbar.active {
@@ -1335,7 +1356,7 @@ const applySelectedTheme = (colorValue) => {
                 fill: #bbb !important;
             }
 	    
-	    .dark-theme .dropdown-item.selected:not(*[data-default]):before {
+            .dark-theme .dropdown-item.selected:not(*[data-default]):before {
                 background-color: #707070;
             }
 
@@ -1385,7 +1406,7 @@ const applySelectedTheme = (colorValue) => {
                 color: var(--whitishColor-dark);
             }
 	    
-	    .clearButton{
+            .clearButton{
                 color: #d6d6d6;
             }
 
@@ -1404,7 +1425,7 @@ const applySelectedTheme = (colorValue) => {
             .dark-theme .backupRestoreBtn:hover,
             .dark-theme .uploadButton:hover,
             .dark-theme .randomButton:hover,
-	    .dark-theme #todoAdd:hover {
+            .dark-theme #todoAdd:hover {
                 background-color: var(--bg-color-dark);
             }
             
@@ -1415,8 +1436,12 @@ const applySelectedTheme = (colorValue) => {
                 background-color: #0e0e0e;
             }
 	    
-	    .dark-theme .todolistitem span {
+            .dark-theme .todolistitem .todoremovebtn {
                 color:#616161;
+            }
+
+	    .dark-theme .todolistitem .todoremovebtn:hover {
+                color:#888888;
             }
 
      	    .dark-theme .micIcon {
