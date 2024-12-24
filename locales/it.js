741a38f1ea162f063d507b0e8d0eaefef69760f7
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2024-12-24 22:16:29 +0530

    Update it.js

--- locales/it.js
+++ locales/it.js
@@ -1,3 +1,4 @@
+// Italian
 const it = {
     // Menu Items
     "feedback": "Feedback",
@@ -83,6 +84,20 @@ const it = {
         "evening": "Buona sera!"
     },
 
+    // Search Engines and rest
+    "googleEngine": "Google",
+    "duckEngine": "Duck",
+    "bingEngine": "Bing",
+    "braveEngine": "Brave",
+    "youtubeEngine": "YouTube",
+    "chatGPT": "ChatGPT",
+    "gemini": "Gemini",
+    "copilot": "Copilot",
+    "perplexity": "Perplexity",
+    "firefly": "Adobe Firefly",
+    "metaAI": "Meta AI",
+    "github": "GitHub",
+
     // Wallpaper and alerts
     "uploadWallpaperText": "Carica immagine",
     "backupText": "Backup",
@@ -94,11 +109,7 @@ const it = {
     "confirmRestore": "Sei sicuro di voler reimpostare le impostazioni? Questa azione non può essere annullata.",
     "Nobackgroundset": "Nessuna immagine di sfondo è attualmente impostata.",
     "clearbackgroundimage": "Sei sicuro di voler rimuovere l'immagine di sfondo?",
-    "confirmbackup": "Sei sicuro di voler eseguire il backup delle tue impostazioni?",
     "ProxyDisclaimer": "Tutte le funzionalità del proxy sono disattivate di default.\n\nSe abiliti i suggerimenti di ricerca e il proxy per il bypass CORS, è fortemente consigliato usare un proprio proxy per una maggiore privacy.\n\nIl proxy predefinito sarà configurato su https://mynt-proxy.rhythmcorehq.com, il che significa che tutti i tuoi dati passeranno attraverso questo servizio, con possibili rischi per la privacy.",
-    "endlink": "Non ci dovrebbe essere uno / alla fine del link",
-    "onlylinks": "Solo link (che iniziano con http:// o https://) sono consentiti.",
-    "imagedimensions": "Attenzione: Le dimensioni dell'immagine caricata ({width}x{height}) superano i 1920x1080 pixel. \n\nCiò potrebbe influire sulle prestazioni o l'immagine potrebbe non essere caricata correttamente.",
     "failedbackup": "Backup fallito: ",
     "restorecompleted": "Ripristino completato con successo!",
     "restorefailed": "Ripristino fallito: "
