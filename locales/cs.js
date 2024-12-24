55dff64a90393d3621de06080625760f65a8b3d3
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2024-12-24 22:14:10 +0530

    Update cs.js

--- locales/cs.js
+++ locales/cs.js
@@ -89,6 +89,20 @@ const cs = {
         "evening": "Dobrý večer!"
     },
 
+    // Search Engines and rest
+    "googleEngine": "Google",
+    "duckEngine": "Duck",
+    "bingEngine": "Bing",
+    "braveEngine": "Brave",
+    "youtubeEngine": "YouTube",
+    "chatGPT": "ChatGPT",
+    "gemini": "Gemini",
+    "copilot": "Copilot",
+    "perplexity": "Perplexity",
+    "firefly": "Adobe Firefly",
+    "metaAI": "Meta AI",
+    "github": "GitHub",
+
     // Wallpaper and alerts
     "uploadWallpaperText": "Nahrát tapetu",
     "backupText": "Zálohovat",
@@ -100,11 +114,7 @@ const cs = {
     "confirmRestore": "Opravdu si přejete resetovat nastavení? Tuto akci nelze zvrátit.",
     "Nobackgroundset": "Aktuálně nemáte nastavený žádný obrázek na pozadí.",
     "clearbackgroundimage": "Opravdu si přejete smazat obrázek na pozadí?",
-    "confirmbackup": "Opravdu si přejete zálohovat nastavení?",
     "ProxyDisclaimer": "Všechny funkce proxy jsou ve výchozím nastavení vypnuté.\n\nPokud zapnete našeptávání ve vyhledávání s proxy pro obcházení CORS, pro lepší zabezpečení vám důrazně doporučujeme hostovat si vlastní proxy.\n\nVe výchozím nastavení je proxy nastavena na https://mynt-proxy.rhythmcorehq.com, což znamená, že všechna vaše data procházejí skrz tuto službu. To může vyvolat obavy o soukromí.",
-    "endlink": "Na konci odkazu by nemělo být / (lomítko)",
-    "onlylinks": "Jsou povolené pouze odkazy (začínající na http:// nebo https://).",
-    "imagedimensions": "Varování: Rozměry nahraného obrázku ({width}x{height}) překračují 1920x1080 pixelů.\n\nTo může ovlivnit výkonnost nebo se obrázek nemusí správně načítat.",
     "failedbackup": "Zálohování selhalo: ",
     "restorecompleted": "Obnova ze zálohy byla úspěšně dokončena!",
     "restorefailed": "Obnova ze zálohy selhala: ",
