ecd4486cb9952712dc523063097f362462e2ce76
ThunderBlaze <darkthunderstormshiva@gmail.com>
2024-12-26 17:22:13 +0530

    Fixed Toggle

--- script.js
+++ script.js
@@ -323,12 +323,11 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     bookmarkViewGrid.addEventListener('click', function() {
-        bookmarkGridCheckbox.click();
+        if (!bookmarkGridCheckbox.checked) bookmarkGridCheckbox.click();
     });
 
     bookmarkViewList.addEventListener('click', function() {
-        bookmarkGridCheckbox.click();
-        // bookmarkGridCheckbox.checked = false;
+        if (bookmarkGridCheckbox.checked) bookmarkGridCheckbox.click();
     });
 
     document.addEventListener('click', function(event) {
@@ -2650,6 +2649,10 @@ document.addEventListener("DOMContentLoaded", function () {
     function loadCheckboxState(key, checkbox) {
         const savedState = localStorage.getItem(key);
         checkbox.checked = savedState === "checked";
+        if (key === "bookmarkGridCheckboxState"&&savedState===undefined) {
+            checkbox.checked = true;
+            localStorage.setItem(key, "checked");
+        }
     }
 
     // Function to save display status to localStorage
