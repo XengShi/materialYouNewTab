175bbbbd90debc7914a60b10cd7feae4b4051526
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2024-12-26 10:59:43 +0530

    Update bn.js

--- locales/bn.js
+++ locales/bn.js
@@ -21,107 +21,107 @@ const bn = {
     "googleAppsMenuText": "গুগল অ্যাপস",
     "googleAppsMenuInfo": "গুগল অ্যাপসের শর্টকাট প্রদর্শন করুন",
 
-        // Digital Clock
-        "digitalclocktittle": "ডিজিটাল ঘড়ি",
-        "digitalclockinfo": "ডিজিটাল ঘড়িতে পরিবর্তন করুন",
-        "timeformattittle": "১২-ঘণ্টা ফরম্যাট",
-        "timeformatinfo": "১২-ঘণ্টার সময় ফরম্যাট ব্যবহার করুন",
-        "greetingtittle": "অভিবাদন",
-        "greetinginfo": "কাস্টম টেক্সটের নিচে অভিবাদন দেখান",
-        "todoListText": "টু ডু লিস্ট",
-        "todoListInfo": "দৈনিক কাজের তালিকা দেখুন",
+    // Digital Clock
+    "digitalclocktittle": "ডিজিটাল ঘড়ি",
+    "digitalclockinfo": "ডিজিটাল ঘড়িতে পরিবর্তন করুন",
+    "timeformattittle": "১২-ঘণ্টা ফরম্যাট",
+    "timeformatinfo": "১২-ঘণ্টার সময় ফরম্যাট ব্যবহার করুন",
+    "greetingtittle": "অভিবাদন",
+    "greetinginfo": "কাস্টম টেক্সটের নিচে অভিবাদন দেখান",
+    "todoListText": "টু ডু লিস্ট",
+    "todoListInfo": "দৈনিক কাজের তালিকা দেখুন",
 
-        // Misc
-        "userTextTitle": "কাস্টমাইজেবল টেক্সট",
-        "userTextInfo": "ঘড়ির নিচে কাস্টম টেক্সট দেখান",
-        "fahrenheitCelsiusCheckbox": "ফারেনহাইটে পরিবর্তন করুন",
-        "fahrenheitCelsiusText": "পরিবর্তন প্রয়োগ করতে পেজ রিফ্রেশ করুন",
-        "micIconTitle": "মাইক্রোফোন আইকন লুকান",
-        "micIconInfo": "যদি ভয়েস টাইপিং কাজ না করে",
-        "hideSearchWith": "সার্চ ইঞ্জিন লুকান",
-        "hideSearchWithInfo": "সার্চ ইঞ্জিনের আইকনে ক্লিক করে সার্চ ইঞ্জিন পরিবর্তন করুন",
-        "search_suggestions_button": "সার্চ সাজেশন",
-        "search_suggestions_text": "সার্চ সাজেশন সক্রিয় করুন",
+    // Misc
+    "userTextTitle": "কাস্টমাইজেবল টেক্সট",
+    "userTextInfo": "ঘড়ির নিচে কাস্টম টেক্সট দেখান",
+    "fahrenheitCelsiusCheckbox": "ফারেনহাইটে পরিবর্তন করুন",
+    "fahrenheitCelsiusText": "পরিবর্তন প্রয়োগ করতে পেজ রিফ্রেশ করুন",
+    "micIconTitle": "মাইক্রোফোন আইকন লুকান",
+    "micIconInfo": "যদি ভয়েস টাইপিং কাজ না করে",
+    "hideSearchWith": "সার্চ ইঞ্জিন লুকান",
+    "hideSearchWithInfo": "সার্চ ইঞ্জিনের আইকনে ক্লিক করে সার্চ ইঞ্জিন পরিবর্তন করুন",
+    "search_suggestions_button": "সার্চ সাজেশন",
+    "search_suggestions_text": "সার্চ সাজেশন সক্রিয় করুন",
 
-        // Proxy
-        "useproxytitletext": "প্রক্সি বাইপাস",
-        "useproxyText": "যদি সার্চ সাজেশন কাজ না করে",
-        "ProxyText": "CORS বাইপাস প্রক্সি",
-        "ProxySubtext": "নিজস্ব CORS বাইপাস প্রক্সি যোগ করুন",
-        "HostproxyButton": "নিজস্ব প্রক্সি হোস্ট করুন",
-        "saveproxy": "সংরক্ষণ করুন",
+    // Proxy
+    "useproxytitletext": "প্রক্সি বাইপাস",
+    "useproxyText": "যদি সার্চ সাজেশন কাজ না করে",
+    "ProxyText": "CORS বাইপাস প্রক্সি",
+    "ProxySubtext": "নিজস্ব CORS বাইপাস প্রক্সি যোগ করুন",
+    "HostproxyButton": "নিজস্ব প্রক্সি হোস্ট করুন",
+    "saveproxy": "সংরক্ষণ করুন",
 
-        // Location
-        "UserLocText": "আপনার অবস্থান লিখুন",
-        "UserLocSubtext": "যদি আবহাওয়া অবস্থান সঠিক না হয়",
-        "userLoc": "আপনার শহর বা স্থানাঙ্ক (অক্ষাংশ, দ্রাঘিমাংশ)",
-        "InputOptionsButton": "ইনপুট অপশন",
-        "saveLoc": "সংরক্ষণ করুন",
+    // Location
+    "UserLocText": "আপনার অবস্থান লিখুন",
+    "UserLocSubtext": "যদি আবহাওয়া অবস্থান সঠিক না হয়",
+    "userLoc": "আপনার শহর বা স্থানাঙ্ক (অক্ষাংশ, দ্রাঘিমাংশ)",
+    "InputOptionsButton": "ইনপুট অপশন",
+    "saveLoc": "সংরক্ষণ করুন",
 
-        // Weather
-        "WeatherApiText": "আপনার WeatherAPI কী লিখুন",
-        "WeatherApiSubtext": "যদি আবহাওয়ার ফাংশন কাজ না করে",
-        "userAPI": "আপনার WeatherAPI কী",
-        "LearnMoreButton": "আরও জানুন",
-        "saveAPI": "সংরক্ষণ করুন",
+    // Weather
+    "WeatherApiText": "আপনার WeatherAPI কী লিখুন",
+    "WeatherApiSubtext": "যদি আবহাওয়ার ফাংশন কাজ না করে",
+    "userAPI": "আপনার WeatherAPI কী",
+    "LearnMoreButton": "আরও জানুন",
+    "saveAPI": "সংরক্ষণ করুন",
 
-        // Body Items
-        // Calendar
-        "days": ['রবি', 'সোম', 'মঙ্গল', 'বুধ', 'বৃহস্পতি', 'শুক্র', 'শনি'],   // Truncated for display
-        //"days": ['রবিবার', 'সোমবার', 'মঙ্গলবার', 'বুধবার', 'বৃহস্পতিবার', 'শুক্রবার', 'শনিবার'],   // Full
-        "months": ['জানুয়ারি', 'ফেব্রুয়ারি', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'আগস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর'],
-        // "months": ['জানু', 'ফেব্রু', 'মার্চ', 'এপ্রি', 'মে', 'জুন', 'জুলাই', 'আগ', 'সেপ্টে', 'অক্টো', 'নভে', 'ডিসে'],   // Truncated
+    // Body Items
+    // Calendar
+    "days": ['রবি', 'সোম', 'মঙ্গল', 'বুধ', 'বৃহস্পতি', 'শুক্র', 'শনি'],   // Truncated for display
+    //"days": ['রবিবার', 'সোমবার', 'মঙ্গলবার', 'বুধবার', 'বৃহস্পতিবার', 'শুক্রবার', 'শনিবার'],   // Full
+    "months": ['জানুয়ারি', 'ফেব্রুয়ারি', 'মার্চ', 'এপ্রিল', 'মে', 'জুন', 'জুলাই', 'আগস্ট', 'সেপ্টেম্বর', 'অক্টোবর', 'নভেম্বর', 'ডিসেম্বর'],
+    // "months": ['জানু', 'ফেব্রু', 'মার্চ', 'এপ্রি', 'মে', 'জুন', 'জুলাই', 'আগ', 'সেপ্টে', 'অক্টো', 'নভে', 'ডিসে'],   // Truncated
 
-        // Weather
-        "humidityLevel": "আর্দ্রতা",
-        "feelsLike": "অনুভূতি হয়",
-        "location": "পৃথিবী",
+    // Weather
+    "humidityLevel": "আর্দ্রতা",
+    "feelsLike": "অনুভূতি হয়",
+    "location": "পৃথিবী",
 
-        // New Tab Item
-        "conditionText": "হ্যালো! আজ আপনি কেমন আছেন?",
-        "enterBtn": "সার্চ করুন",
-        "searchPlaceholder": "এখানে টাইপ করুন...",
-        "listenPlaceholder": "শোনা হচ্ছে...",
-        "todoPlaceholder": "টাস্ক যোগ করুন...",
-        "searchWithHint": "সার্চের মাধ্যম",
-        "ai_tools": "এআই টুলস",
-        "userText": "এখানে আপনার টেক্সট লিখুন",
-        "googleAppsHover": "গুগল অ্যাপস",
-        "todoListHover": "টু ডু লিস্ট",
+    // New Tab Item
+    "conditionText": "হ্যালো! আজ আপনি কেমন আছেন?",
+    "enterBtn": "সার্চ করুন",
+    "searchPlaceholder": "এখানে টাইপ করুন...",
+    "listenPlaceholder": "শোনা হচ্ছে...",
+    "todoPlaceholder": "টাস্ক যোগ করুন...",
+    "searchWithHint": "সার্চের মাধ্যম",
+    "ai_tools": "এআই টুলস",
+    "userText": "এখানে আপনার টেক্সট লিখুন",
+    "googleAppsHover": "গুগল অ্যাপস",
+    "todoListHover": "টু ডু লিস্ট",
 
-        // Bookmarks
-        "bookmarksHeading": "বুকমার্কস",
-        "bookmarkViewAs" : "এইভাবে দেখুন",
-        "bookmarkViewGrid" : "গ্রিড",
-        "bookmarkViewList" : "লিস্ট",
-        "bookmarkSearch": "বুকমার্ক সার্চ করুন",
+    // Bookmarks
+    "bookmarksHeading": "বুকমার্কস",
+    "bookmarkViewAs": "এইভাবে দেখুন",
+    "bookmarkViewGrid": "গ্রিড",
+    "bookmarkViewList": "লিস্ট",
+    "bookmarkSearch": "বুকমার্ক সার্চ করুন",
 
-        // Greeting
-        greeting: {
-            "morning": "শুভ সকাল!",
-            "afternoon": "শুভ বিকেল!",
-            "evening": "শুভ সন্ধ্যা!"
-        },
+    // Greeting
+    greeting: {
+        "morning": "শুভ সকাল!",
+        "afternoon": "শুভ বিকেল!",
+        "evening": "শুভ সন্ধ্যা!"
+    },
 
-        // Search Engines and rest
-        "googleEngine": "গুগল",
-        "duckEngine": "ডাকডাকগো",
-        "bingEngine": "বিং",
-        "braveEngine": "ব্রেভ",
-        "youtubeEngine": "ইউটিউব",
-        "chatGPT": "চ্যাটজিপিটি",
-        "gemini": "জেমিনি",
-        "copilot": "কোপাইলট",
-        "perplexity": "পারপ্লেক্সিটি",
-        "firefly": "অ্যাডোবি ফায়ারফ্লাই",
-        "metaAI": "মেটা এআই",
-        "github": "গিটহাব",
+    // Search Engines and rest
+    "googleEngine": "গুগল",
+    "duckEngine": "ডাকডাকগো",
+    "bingEngine": "বিং",
+    "braveEngine": "ব্রেভ",
+    "youtubeEngine": "ইউটিউব",
+    "chatGPT": "চ্যাটজিপিটি",
+    "gemini": "জেমিনি",
+    "copilot": "কোপাইলট",
+    "perplexity": "পারপ্লেক্সিটি",
+    "firefly": "অ্যাডোবি ফায়ারফ্লাই",
+    "metaAI": "মেটা এআই",
+    "github": "গিটহাব",
 
-        // Wallpaper and alerts
-        "uploadWallpaperText": "ওয়ালপেপার দিন", //"ওয়ালপেপার আপলোড করুন"
-        "backupText": "ব্যাকআপ করুন",
-        "restoreText": "পুনরুদ্ধার করুন",
-        "rangColor": "রঙ নির্বাচন করুন",
+    // Wallpaper and alerts
+    "uploadWallpaperText": "ওয়ালপেপার দিন", //"ওয়ালপেপার আপলোড করুন"
+    "backupText": "ব্যাকআপ করুন",
+    "restoreText": "পুনরুদ্ধার করুন",
+    "rangColor": "রঙ নির্বাচন করুন",
 
     // Dialog boxes (alerts)
     "confirmWallpaper": "আপনি কি আজকের জন্য একটি নতুন ছবি ওয়ালপেপার হিসেবে সেট করতে চান?",
@@ -134,4 +134,5 @@ const bn = {
     "restorefailed": "পুনরুদ্ধার ব্যর্থ: ",
     "invalidBackup": "অবৈধ ব্যাকআপ ফাইল",
     "deleteBookmark": "আপনি কি নিশ্চিত যে আপনি বুকমার্কটি মুছে ফেলতে চান?",
+    "UnsupportedBrowser": "আপনার ব্রাউজারে বুকমার্ক সমর্থিত নয়",
 }
