ee21b1ba0dfd045fd93f80b8fa8b7a382f674e08
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2025-01-01 18:35:41 +0530

    Update az.js

--- locales/az.js
+++ locales/az.js
@@ -90,6 +90,7 @@ const az = {
     "chatGPT": "ChatGPT",
     "gemini": "Gemini",
     "copilot": "Copilot",
+    "claude": "Claude",
     "perplexity": "Perplexity",
     "firefly": "Adobe Firefly",
     "metaAI": "Meta AI",
