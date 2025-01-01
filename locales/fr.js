2f65d6ddf2dc548ab957bd3b9665260434480744
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2025-01-01 18:37:06 +0530

    Update fr.js

--- locales/fr.js
+++ locales/fr.js
@@ -109,6 +109,7 @@ const fr = {
     "chatGPT": "ChatGPT",
     "gemini": "Gemini",
     "copilot": "Copilot",
+    "claude": "Claude",
     "perplexity": "Perplexity",
     "firefly": "Adobe Firefly",
     "metaAI": "Meta AI",
