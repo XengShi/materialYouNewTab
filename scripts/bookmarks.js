6def86ece0ed625bb713617e9e844187a41ba1e7
Prem Kumar <60751338+prem-k-r@users.noreply.github.com>
2025-01-14 19:55:49 +0530

    Update bookmarks.js

--- scripts/bookmarks.js
+++ scripts/bookmarks.js
@@ -1,10 +1,10 @@
-<!--
-     Material You NewTab
-     Copyright (c) 2023-2025 XengShi
-     Licensed under the GNU General Public License v3.0 (GPL-3.0)
-     You should have received a copy of the GNU General Public License along with this program.
-     If not, see <https://www.gnu.org/licenses/>.
--->
+/* 
+ * Material You NewTab
+ * Copyright (c) 2023-2025 XengShi
+ * Licensed under the GNU General Public License v3.0 (GPL-3.0)
+ * You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
 
 // ------------------------ Bookmark System -----------------------------------
 // DOM Variables
