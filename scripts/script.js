8d98d9cbfcac669dcec5f629d5c322ef708d3d52
Prem Kumar <prem12321kumar@gmail.com>
2025-02-12 00:56:17 +0530

    split search-suggestions

--- scripts/script.js
+++ scripts/script.js
@@ -7,54 +7,12 @@
  */
 
 
-// TODO: Seperate stuffs to theme.js, search.js, search-suggestions.js (with proxy)
+// TODO: Seperate stuffs to theme.js, search.js
 // TODO: `clock.js` - might be divided in two: `clock-analog.js` and `clock-digital.js`
 // TODO: `search.js` - might be divided in two: `search-default.js` and `search-dropdown.js`
 // TODO: Move all the CSS in a file called `theme/theme.css` (theme is the folder name) ??
 // TODO: Move all the SVG icons in files called `svgs/icon-name.svg` ??
 
-
-let proxyurl;
-document.addEventListener("DOMContentLoaded", () => {
-    const userProxyInput = document.getElementById("userproxy");
-    const saveProxyButton = document.getElementById("saveproxy");
-    const savedProxy = localStorage.getItem("proxy");
-
-    const defaultProxyURL = "https://mynt-proxy.rhythmcorehq.com"; //Default proxy url
-    if (savedProxy && savedProxy !== defaultProxyURL) {
-        userProxyInput.value = savedProxy;
-    }
-
-    userProxyInput.addEventListener("keydown", (event) => handleEnterPress(event, "saveproxy"));
-
-    // Save the proxy to localStorage
-    saveProxyButton.addEventListener("click", () => {
-        proxyurl = userProxyInput.value.trim();
-
-        // If the input is empty, use the default proxy.
-        if (proxyurl === "") {
-            proxyurl = defaultProxyURL;
-        } else {
-            // Validate if input starts with "http://" or "https://"
-            if (!(proxyurl.startsWith("http://") || proxyurl.startsWith("https://"))) {
-                // Automatically correct input by adding "http:/"" if not present
-                proxyurl = "http://" + proxyurl;
-            }
-
-            // Remove trailing slash if exists
-            if (proxyurl.endsWith("/")) {
-                proxyurl = proxyurl.slice(0, -1);  // Remove the last character ("/")
-            }
-        }
-        // Set the proxy in localStorage, clear the input, and reload the page
-        localStorage.setItem("proxy", proxyurl);
-        userProxyInput.value = "";
-        location.reload();
-    });
-
-    // Determine which proxy URL to use
-    proxyurl = savedProxy || defaultProxyURL;
-});
 
 // --------------------------- Search Bar ------------------------------------
 
@@ -91,7 +49,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 
-    document.querySelector(".dropdown-btn").addEventListener("click", function (event) {
+    document.querySelector(".dropdown-btn").addEventListener("click", function () {
         const resultBox = document.getElementById("resultBox");
         if (resultBox.classList.toString().includes("show")) return;
 
@@ -526,189 +484,6 @@ colorPicker.addEventListener("input", handleColorPickerChange);
 // End of Function to apply the selected theme
 
 
-// ------------Search Suggestions---------------
-
-// Show the result box
-function showResultBox() {
-    resultBox.classList.add("show");
-    resultBox.style.display = "block";
-}
-
-// Hide the result box
-function hideResultBox() {
-    resultBox.classList.remove("show");
-    //resultBox.style.display = "none";
-}
-
-showResultBox();
-hideResultBox();
-
-document.getElementById("searchQ").addEventListener("input", async function () {
-    const searchsuggestionscheckbox = document.getElementById("searchsuggestionscheckbox");
-    if (searchsuggestionscheckbox.checked) {
-        var selectedOption = document.querySelector("input[name='search-engine']:checked").value;
-        var searchEngines = {
-            engine1: "https://www.google.com/search?q=",
-            engine2: "https://duckduckgo.com/?q=",
-            engine3: "https://bing.com/?q=",
-            engine4: "https://search.brave.com/search?q=",
-            engine5: "https://www.youtube.com/results?search_query="
-        };
-        const query = this.value;
-        const resultBox = document.getElementById("resultBox");
-
-        if (query.length > 0) {
-            try {
-                // Fetch autocomplete suggestions
-                const suggestions = await getAutocompleteSuggestions(query);
-
-                if (suggestions === "") {
-                    hideResultBox();
-                } else {
-                    // Clear the result box
-                    resultBox.innerHTML = "";
-
-                    // Add suggestions to the result box
-                    suggestions.forEach((suggestion, index) => {
-                        const resultItem = document.createElement("div");
-                        resultItem.classList.add("resultItem");
-                        resultItem.textContent = suggestion;
-                        resultItem.setAttribute("data-index", index);
-                        resultItem.onclick = () => {
-                            var resultlink = searchEngines[selectedOption] + encodeURIComponent(suggestion);
-                            window.location.href = resultlink;
-                        };
-                        resultBox.appendChild(resultItem);
-                    });
-
-                    // Check if the dropdown of search shortcut is open
-                    const dropdown = document.querySelector(".dropdown-content");
-
-                    if (dropdown.style.display === "block") {
-                        dropdown.style.display = "none";
-                    }
-                    showResultBox();
-                }
-            } catch (error) {
-                // Handle the error (if needed)
-            }
-        } else {
-            hideResultBox();
-        }
-    }
-});
-
-let isMouseOverResultBox = false;
-// Track mouse entry and exit within the resultBox
-resultBox.addEventListener("mouseenter", () => {
-    isMouseOverResultBox = true;
-    // Remove keyboard highlight
-    const activeItem = resultBox.querySelector(".active");
-    if (activeItem) {
-        activeItem.classList.remove("active");
-    }
-});
-
-resultBox.addEventListener("mouseleave", () => {
-    isMouseOverResultBox = false;
-});
-
-document.getElementById("searchQ").addEventListener("keydown", function (e) {
-    if (isMouseOverResultBox) {
-        return; // Ignore keyboard events if the mouse is in the resultBox
-    }
-    const activeItem = resultBox.querySelector(".active");
-    let currentIndex = activeItem ? parseInt(activeItem.getAttribute("data-index")) : -1;
-
-    if (resultBox.children.length > 0) {
-        if (e.key === "ArrowDown") {
-            e.preventDefault();
-            if (activeItem) {
-                activeItem.classList.remove("active");
-            }
-            currentIndex = (currentIndex + 1) % resultBox.children.length;
-            resultBox.children[currentIndex].classList.add("active");
-
-            // Ensure the active item is visible within the result box
-            const activeElement = resultBox.children[currentIndex];
-            activeElement.scrollIntoView({ block: "nearest" });
-        } else if (e.key === "ArrowUp") {
-            e.preventDefault();
-            if (activeItem) {
-                activeItem.classList.remove("active");
-            }
-            currentIndex = (currentIndex - 1 + resultBox.children.length) % resultBox.children.length;
-            resultBox.children[currentIndex].classList.add("active");
-
-            // Ensure the active item is visible within the result box
-            const activeElement = resultBox.children[currentIndex];
-            activeElement.scrollIntoView({ block: "nearest" });
-        } else if (e.key === "Enter" && activeItem) {
-            e.preventDefault();
-            activeItem.click();
-        }
-    }
-});
-
-// Check for different browsers and return the corresponding client parameter
-function getClientParam() {
-    if (isFirefox) return "firefox";
-    if (isOpera) return "opera";
-    if (isChromiumBased) return "chrome";
-    if (isSafari) return "safari";
-    return "firefox"; // Default to Firefox if the browser is not recognized
-}
-
-async function getAutocompleteSuggestions(query) {
-    const clientParam = getClientParam(); // Get the browser client parameter dynamically
-    var selectedOption = document.querySelector('input[name="search-engine"]:checked').value;
-    var searchEnginesapi = {
-        engine1: `https://www.google.com/complete/search?client=${clientParam}&q=${encodeURIComponent(query)}`,
-        engine2: `https://duckduckgo.com/ac/?q=${encodeURIComponent(query)}&type=list`,
-        engine3: `https://www.google.com/complete/search?client=${clientParam}&q=${encodeURIComponent(query)}`,
-        engine4: `https://search.brave.com/api/suggest?q=${encodeURIComponent(query)}&rich=true&source=web`,
-        engine5: `https://www.google.com/complete/search?client=${clientParam}&ds=yt&q=${encodeURIComponent(query)}`
-    };
-    const useproxyCheckbox = document.getElementById("useproxyCheckbox");
-    let apiUrl = searchEnginesapi[selectedOption];
-    if (useproxyCheckbox.checked) {
-        apiUrl = `${proxyurl}/proxy?url=${encodeURIComponent(apiUrl)}`;
-    }
-
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-
-        if (selectedOption === "engine4") {
-            const suggestions = data[1].map(item => {
-                if (item.is_entity) {
-                    return `${item.q} - ${item.name} (${item.category ? item.category : "No category"})`;
-                } else {
-                    return item.q;
-                }
-            });
-            return suggestions;
-        } else {
-
-            return data[1];
-        }
-    } catch (error) {
-        console.error("Error fetching autocomplete suggestions:", error);
-        return [];
-    }
-}
-
-// Hide results when clicking outside
-document.addEventListener("click", function (event) {
-    const searchbar = document.getElementById("searchbar");
-    // const resultBox = document.getElementById("resultBox");
-
-    if (!searchbar.contains(event.target)) {
-        hideResultBox();
-    }
-});
-// ------------End of Search Suggestions---------------
-
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -765,62 +540,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     initShortCutSwitch(element);
-
-
-    // Search Suggestions
-    const searchsuggestionscheckbox = document.getElementById("searchsuggestionscheckbox");
-    const proxybypassField = document.getElementById("proxybypassField");
-    const proxyinputField = document.getElementById("proxyField");
-    const useproxyCheckbox = document.getElementById("useproxyCheckbox");
-
-    // This function shows the proxy disclaimer.
-    function showProxyDisclaimer() {
-        const message = translations[currentLanguage]?.ProxyDisclaimer || translations["en"].ProxyDisclaimer;
-        return confirm(message);
-    }
-
-    // Add change event listeners for the checkboxes
-    searchsuggestionscheckbox.addEventListener("change", function () {
-        saveCheckboxState("searchsuggestionscheckboxState", searchsuggestionscheckbox);
-        if (searchsuggestionscheckbox.checked) {
-            proxybypassField.classList.remove("inactive");
-            saveActiveStatus("proxybypassField", "active");
-        } else {
-            proxybypassField.classList.add("inactive");
-            saveActiveStatus("proxybypassField", "inactive");
-            useproxyCheckbox.checked = false;
-            saveCheckboxState("useproxyCheckboxState", useproxyCheckbox);
-            proxyinputField.classList.add("inactive");
-            saveActiveStatus("proxyinputField", "inactive");
-        }
-    });
-
-    useproxyCheckbox.addEventListener("change", function () {
-        if (useproxyCheckbox.checked) {
-            // Show the disclaimer and check the user's choice
-            const userConfirmed = showProxyDisclaimer();
-            if (userConfirmed) {
-                // Only enable the proxy if the user confirmed
-                saveCheckboxState("useproxyCheckboxState", useproxyCheckbox);
-                proxyinputField.classList.remove("inactive");
-                saveActiveStatus("proxyinputField", "active");
-            } else {
-                // Revert the checkbox state if the user did not confirm
-                useproxyCheckbox.checked = false;
-            }
-        } else {
-            // If the checkbox is unchecked, disable the proxy
-            saveCheckboxState("useproxyCheckboxState", useproxyCheckbox);
-            proxyinputField.classList.add("inactive");
-            saveActiveStatus("proxyinputField", "inactive");
-        }
-    });
-
-    // Load and apply the saved checkbox states and display statuses
-    loadCheckboxState("searchsuggestionscheckboxState", searchsuggestionscheckbox);
-    loadCheckboxState("useproxyCheckboxState", useproxyCheckbox);
-    loadActiveStatus("proxyinputField", proxyinputField);
-    loadActiveStatus("proxybypassField", proxybypassField);
 });
 
 document.addEventListener("keydown", function (event) {
