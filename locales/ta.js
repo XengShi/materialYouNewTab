1ab4e0ed44460de4b07631972a7e924bd6982f35
தமிழ் நேரம் <anishprabu.t@gmail.com>
2025-06-19 09:05:43 +0530

    Tamil Translation updated

---
+++ locales/ta.js
@@ -0,0 +1,196 @@
+// தமிழ்
+const ta = {
+    // Menu Items
+    "github": "அறிவிலிமையம்",
+    "feedback": "Feedback",
+
+    // Shortcuts
+    "shortcutsText": "Shortcuts",
+    "enableShortcutsText": "Show saved shortcuts",
+    "editShortcutsText": "Edit Shortcuts",
+    "shortcutsInfoText": "Choose which shortcuts get shown",
+    "editShortcutsList": "Saved Shortcuts",
+    "editShortcutsListInfo": "You can add new shortcuts by clicking the \"+\" icon or edit existing ones by clicking on the shortcut name or URL.",
+    "adaptiveIconText": "Adaptive Icons",
+    "adaptiveIconInfoText": "Shortcut icons will adapt to theme",
+    "bookmarksText": "Bookmarks",
+    "bookmarksInfo": "Show bookmarks sidebar",
+    "ai_tools_button": "AI Tools",
+    "enable_ai_tools": "Show shortcuts for AI tools",
+    "aiToolsSettingsText": "AI Tools Settings",
+    "aiToolsSettingsInfo": "Manage AI tools shortcuts",
+    "googleAppsMenuText": "Google Apps",
+    "googleAppsMenuInfo": "Show shortcuts for Google Apps",
+    "googleAppsHover": "Google Apps",    // Keep this shorter
+
+    // To-do List
+    "todoListText": "To Do List",
+    "todoListInfo": "Show a daily To Do list",
+    "todoListHover": "ToDo List",    // Keep this short
+    "todoPlaceholder": "Add task...",
+
+    // Clock
+    "hideClockBox": "Hide Clock",
+    "hideClockBoxInfo": "Hide the clock and date",
+    "digitalclocktitle": "Digital Clock",
+    "digitalclockinfo": "Switch to the digital clock",
+    "timeformattitle": "12-Hour Format",
+    "timeformatinfo": "Use 12-hour time format",
+    "greetingtitle": "Greeting",
+    "greetinginfo": "Show greeting below custom text",
+
+    // Misc
+    "userTextTitle": "Customizable Text",
+    "userTextInfo": "Show custom text below the clock",
+    "fahrenheitCelsiusCheckbox": "Switch to Fahrenheit",
+    "fahrenheitCelsiusText": "Refresh the page to apply changes",
+    "hideWeatherTitle": "Hide Weather",
+    "hideWeatherInfo": "Disable the weather widgets",
+    "hideWeatherBox": "Hide Weather Card",    // Toggle for mobile devices only
+    "hideWeatherBoxInfo": "Show only temperature pill",
+    "minMaxTempText": "Min-Max Temperature",
+    "minMaxTempSubText": "Replace 'Feels Like' with minimum and maximum temperature",
+
+    // Search
+    "micIconTitle": "Hide Microphone Icon",
+    "micIconInfo": "If voice typing is not working",
+    "hideSearchWith": "Hide Search Engines",
+    "hideSearchWithInfo": "Switch between search engines by clicking its icon",
+    "motivationalQuotesText": "Motivational Quotes",
+    "motivationalQuotesInfo": "Show quotes below the searchbar",
+    "search_suggestions_button": "Search Suggestions",
+    "search_suggestions_text": "Enable search suggestions",
+
+    // Proxy
+    "useproxytitletext": "Proxy Bypass",
+    "useproxyText": "If search suggestions aren't working",
+    "ProxyText": "CORS Bypass Proxy",
+    "ProxySubtext": "Add your own CORS bypass proxy",
+    "HostproxyButton": "Host your own proxy",
+
+    // இருப்பிடம்
+    "useGPS": "இடம் காட்டியைப் பயன்படுத்து",
+    "useGPSInfo": "நேரடி இருப்பிட புதுப்பிப்புகளுக்கு இடம் காட்டியை இயக்கு",
+    "UserLocText": "உங்கள் இருப்பிடத்தை உள்ளிடவும்",
+    "UserLocSubtext": "வானிலை இருப்பிடம் சரியாக இல்லாவிட்டால்",
+    "userLoc": "உங்கள் நகரம் அல்லது ஆயத்தொலைவுகள் (நிலகிடமட்டக்கோடு, நிலநடுக்கோடு)",
+    "PrivacyPolicy": "தனியுரிமைக் கொள்கை",
+
+    // Weather
+    "WeatherApiText": "உங்கள் WeatherAPI விசையை உள்ளிடவும்",    // 'வானிலைபநிஇ' சரத்திற்கு, ஒலிபெயர்ப்பு மட்டும்
+    "WeatherApiSubtext": "வானிலை செயல்பாடு வேலை செய்யவில்லை என்றால்",
+    "userAPI": "உங்கள் வானிலைபநிஇ விசை",
+    "LearnMoreButton": "மேலும் அறிக",
+    "saveAPI": "சேமி",
+
+    // உடல் உருப்படிகள்
+    // நாள்காட்டி
+    "days": ['ஞாயிறு', 'திங்கள்', 'செவ்வாய்', 'அறிவன்', 'வியாழன்', 'வெள்ளி', 'காரி'],
+    "months": ['மா-தை', 'தை-மாசி', 'மா-பங்குனி', 'ப-சித்திரை', 'சி-வைகாசி', 'வை-ஆனி', 'ஆ-ஆடி', 'ஆ-ஆவணி', 'ஆ-புரட்டாசி', 'பு-ஐப்பசி', 'ஐ-கார்த்திகை', 'கா-மார்கழி'],
+
+    // Bookmarks
+    "bookmarksHeading": "புத்தக்குறிகள்",
+    "bookmarkSortBy": "வரிசைப்படுத்து",
+    "sortAlphabetical": "அ-ஔ",
+    "sortTimeAdded": "பழைய-புதிய",
+    "bookmarkViewAs": "இவ்வாறு காண்க",
+    "bookmarkViewGrid": "கட்டம்", // இதை சிறியதாக வைத்திரு
+    "bookmarkViewList": "பட்டியல்", // இதை சிறியதாக வைத்திரு
+    "bookmarkSearch": "புத்தக்குறியைத் தேடு",
+    "editBookmarkHeading": "புத்தக்குறியைத் திருத்து",
+    "editBookmarkName": "புத்தக்குறியின் பெயர்",
+    "editBookmarkURL": "புத்தக்குறி முகவரி",
+
+    // புதிய தாவல் உருப்படி
+    "conditionText": "வணக்கம்! இன்று நீங்கள் எப்படி இருக்கிறீர்கள்?",
+    "humidityLevel": "ஈரப்பதம்",
+    "feelsLike": "உணர்கிறது",
+    "minMaxTemp": "குறைந்தஅளவு ~ அதிகஅளவு",
+    "location": "பூமி",
+    "enterBtn": "தேடு",
+    "searchPlaceholder": "இங்கே தட்டச்சு செய்க...",
+    "listenPlaceholder": "கேட்கிறேன்...",
+    "searchWithHint": "உடன் தேடு",
+    "searchOnHint": "மேல் தேடு",
+    "userText": "திருத்த இங்கே சொடுக்கு",
+
+    // வாழ்த்து
+    greeting: {
+        "morning": "காலை வணக்கம்!",
+        "afternoon": "பகல் வணக்கம்!",
+        "evening": "மாலை வணக்கம்!"
+    },
+
+    // தேடுபொறிகள்
+    "defaultEngine": "இயல்புநிலை",
+    "googleEngine": "கூகிள்",
+    "duckEngine": "வாத்து",    // வாத்துவாத்துபோ
+    "bingEngine": "பிங்",
+    "braveEngine": "துணிச்சல்",
+    "youtubeEngine": "யூடியூப்",
+    "gImagesEngine": "படங்கள்",  // கூகிள் படங்கள், https://www.google.com/search?q=check&hl=bn
+    "redditEngine": "ரெடிட்",
+    "wikipediaEngine": "விக்கிபீடியா",
+    "quoraEngine": "குரா",
+
+    // செஅ கருவிகள்
+    "ai_tools": "செஅ கருவிகள்",
+    "chatGPT": "அரட்டைசீபிடி",
+    "gemini": "செமினி",
+    "copilot": "துணை விமானி",
+    "claude": "கிளாட்",
+    "grok": "க்ரோக்",
+    "qwen": "க்வென்",
+    "perplexity": "குழப்பம்",
+    "deepseek": "ஆழமாகதேடு",
+    "metaAI": "தரவு செஅ",
+    "firefly": "அடோப் தீஈ",
+    "aiSettingsIntro": "எந்த செஅ கருவிகளைக் காட்ட வேண்டும் என்பதைத் தேர்ந்தெடு",
+    "resetAISettingsBtn": "மீட்டமை",
+
+    // கருப்பொருள்
+    "enableDarkMode": "இருள் முறை(பரிசோதனை)",
+    "enableDarkModeInfo": "இருள் முறை கருப்பொருள்கள் இயக்கு",
+
+    // பின்னணி மற்றும் அமைப்புகள்
+    "uploadWallpaperText": "பின்படத்தை பதிவேற்று",    // இதை சிறியதாக வைத்திரு
+    "rangColor": "நிறம் எடு",    // இதை சிறியதாக வைத்திரு
+    "backupText": "காப்புப்பிரதி",
+    "restoreText": "மீட்டெடு",
+    "resetsettings": "அமைப்புகளை மீட்டமை",
+    "menuCloseText": "மூடு",
+
+    // குறிப்புகள்
+    "switchSearchModes": "தேடல் முறைகளை மாற்று",
+    "switchSearchModesInfo": "பயன்முறையை மாற்ற 'உடன் தேடு' என்பதைக் சொடுக்கு.",
+    "adjustZoom": "பெரிதாக்கத்தை சரிசெய்",
+    "adjustZoomInfo": "பெரிதாக்கத்தை சரிசெய்ய கட்டு + \"+\" அல்லது கட்டு + \"-\" ஐ அழுத்தவும்.",
+    "changeBrowserTheme": "உலாவி கருப்பொருளை மாற்று",
+    "chromeThemeInfo": "மூன்று புள்ளிகளைக் சொடுக்கவும் (⋮) > கூடுதல் கருவிகள் > நிறமியைத் தனிப்பயனாக்கு, பின்னர் நீட்டிப்பு கருப்பொருளுடன் பொருந்தக்கூடிய வண்ணத்தைத் தேர்வு செய்யவும்.",
+    "edgeThemeInfo": "அமைப்புகள் > தோற்றம் என்பதற்குச் சென்று, நீட்டிப்புகருப்பொருளுடன் பொருந்தக்கூடிய வண்ணத்தைத் தேர்வு செய்யவும்.",
+    "braveThemeInfo": '<a href="https://chromewebstore.google.com/category/themes/" target="_blank">நிறமி கருப்பொருள் கடையைப்</a> பார்வையிடவும், நீட்டிப்புடன் பொருந்தக்கூடிய கருப்பொருளைப் பதிவிறக்கிப் பயன்படுத்தவும்.',
+    "firefoxThemeInfo": '<a href="https://addons.mozilla.org/firefox/themes/" target="_blank">தீநரி கருப்பொருள்களைப்</a> பார்வையிடவும், நீட்டிப்புடன் பொருந்தக்கூடிய கருப்பொருளைப் பதிவிறக்கிப் பயன்படுத்தவும்.',
+    "updateFirefoxHomepage": "முகப்புப் பக்கத்தைப் புதுப்பி",
+    "updateFirefoxHomepageInfo": "புதிய தாவலைப் போல உங்கள் முகப்புப் பக்கத்தையும் மாற்ற, <a href=\"https://github.com/XengShi/materialYouNewTab#-firefox\" target=\"_blank\">இந்த இணைப்பைப்</a> பார்வையிட்டு வழிமுறைகளைப் பின்பற்றவும்.",
+    "dontShowTips": "மீண்டும் காட்ட வேண்டாம்",
+
+    // உரையாடல் பெட்டிகள் (முன்னறிவிப்புகள்)
+    "okText": "சரி",
+    "yesText": "ஆம்",
+    "noText": "இல்லை",
+    "agreeText": "ஒப்புக்கொள்கிறேன்",
+    "cancelText": "நீக்கறல்",
+    "confirmWallpaper": "இன்றைய நாளுக்கு உங்கள் பின்னணிபடமாக ஒரு புதிய படத்தை அமைக்க விரும்புகிறீர்களா?",
+    "confirmRestore": "உங்கள் அமைப்புகளை மீட்டமைக்க விரும்புகிறீர்களா? இந்த செயலை செயல்தவிர்க்க முடியாது.",
+    "Nobackgroundset": "தற்போது எந்த பின்னணி படமும் அமைக்கப்படவில்லை.",
+    "clearbackgroundimage": "பின்னணி படத்தை அழிக்க விரும்புகிறீர்களா?",
+    "ProxyDisclaimer": "இயல்பாகவே அனைத்து பதிலாள் நற்பொருத்தங்களும் முடக்கப்பட்டுள்ளன.\n\nநீங்கள் தேடல் பரிந்துரைகளையும் CORS தனிவழி பதிலாள்யையும் இயக்கினால், மேம்பட்ட தனியுரிமைக்காக உங்கள் சொந்த பதிலாளை புரவலன் செய்ய கடுமையாக பரிந்துரைக்கப்படுகிறது.\n\nஇயல்புநிலையாக, பதிலாள் https://mynt-proxy.rhythmcorehq.com ஆக அமைக்கப்படும், அதாவது உங்கள் எல்லா தரவும் இந்த சேவையின் வழியாக செல்லும், இது தனியுரிமை கவலைகளை ஏற்படுத்தக்கூடும்.",
+    "GPSDisclaimer": "உங்கள் இருப்பிடத் தரவு துல்லியமான வானிலை புதுப்பிப்புகளை வழங்க மட்டுமே பயன்படுத்தப்படுகிறது. இது உலாவியில் தற்காலிகமாக சேமிக்கப்படுகிறது மற்றும் எந்த மூன்றாம் தரப்பு சேவைகளுடனும் பகிரப்படவில்லை அல்லது எங்களாலும் அணுக முடியாது.\n\nGPSஐ இயக்குவதன் மூலம், நீட்டிப்புடன் உங்கள் இருப்பிடத்தை உள்ளகத்தில் பகிர ஒப்புக்கொள்கிறீர்கள்.",
+    "failedbackup": "காப்புப்பிரதி தோல்வியடைந்தது: ",
+    "restorecompleted": "மீட்டெடுப்பு வெற்றிகரமாக முடிந்தது!",
+    "restorefailed": "மீட்டெடுப்பு தோல்வியடைந்தது: ",
+    "invalidBackup": "தவறான காப்புப்பிரதி கோப்பு தேர்ந்தெடுக்கப்பட்டது.",
+    "deleteBookmark": "\"{title}\"என்ற புத்தக்குறியை நீக்க விரும்புகிறீர்களா?",  // {title}ஐ மொழிபெயர்க்க வேண்டாம்
+    "UnsupportedBrowser": "உங்கள் உலாவியில் புத்தக்குறிகள் ஆதரிக்கப்படவில்லை.",
+    "resetShortcutsPrompt": "சேமிக்கப்பட்ட அனைத்து குறுக்குவழிகளும் நீக்கப்பட்டு இயல்புநிலைக்கு மீட்டமைக்கப்படும். நீங்கள் தொடர விரும்புகிறீர்களா?",
+};
